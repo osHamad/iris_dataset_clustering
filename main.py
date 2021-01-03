ba95c14b4d83bfe3bc5c75694eec9fe673d93101
Osama Hamad <osamahama2005@gmail.com>
2021-01-03 16:53:35 -0500

    add- clsuters predicted

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from sklearn.cluster import KMeans
 from csv import reader
+from matplotlib import pyplot as plt
 
 def main():
     with open('iris.csv', newline='') as iris_data:
         iris_data = list(reader(iris_data))
 
-    print(iris_data)
+    set_data = [[i[2], i[3]] for i in iris_data]
+
+    km = KMeans(n_clusters=3)
+    predicted = km.fit_predict(set_data)
+    print(predicted)
 
 if __name__ == '__main__':
     main()
